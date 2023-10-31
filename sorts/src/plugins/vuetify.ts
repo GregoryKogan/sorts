4705d3f732eaf1e91da152f42181d009d3bb2529
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-10-31 16:08:17 +0300

    Init Vue3 project

---
+++ sorts/src/plugins/vuetify.ts
@@ -0,0 +1,35 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
+
+// Composables
+import { createVuetify } from 'vuetify';
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    defaultTheme: "dark",
+    themes: {
+      dark: { // tokyo night storm theme
+        colors: {
+          background: "#24283b",
+          primary: "#7aa2f7",
+          secondary: "#bb9af7",
+          surface: "#414868",
+          error: "#f7768e",
+          info: "#7dcfff",
+          success: "#9ece6a",
+          warning: "#e0af68",
+          "on-background": "#c0caf5",
+          "on-surface": "#c0caf5",
+        },
+      },
+    },
+  },
+});

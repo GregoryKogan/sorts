b791043a1bd3d29f3afcb59356c77314e72bb101
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-10-31 16:34:49 +0300

    deploy

---
+++ assets/tag-d04496aa.js
@@ -0,0 +1,1 @@
+import{p as t,g as n}from"./index-92225df0.js";const s=t({class:[String,Array],style:{type:[String,Array,Object],default:null}},"component");function o(e){const r=n("useRender");r.render=e}const p=t({tag:{type:String,default:"div"}},"tag");export{p as a,s as m,o as u};

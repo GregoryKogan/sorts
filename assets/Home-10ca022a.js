245c325784b77c214a2df84cb7a109e17968d57b
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-10-31 16:07:43 +0300

    deploy

---
+++ assets/Home-10ca022a.js
@@ -0,0 +1,1 @@
+import{q as n,z as s}from"./index-5af35702.js";const l=(e,o)=>{const t=e.__vccOpts||e;for(const[c,r]of o)t[c]=r;return t},_={};function a(e,o){return n(),s("h1",null,"Hello, world!")}const m=l(_,[["render",a]]);export{m as default};
